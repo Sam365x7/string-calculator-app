f6fa1c4295029282592f8c5312d2b52f454f8da8
Saami <mrsaamik@gmail.com>
2025-04-07 18:35:31 +0530

    Added support for multple comma seperated values and test cases

--- src/utils/stringCalculator.js
+++ src/utils/stringCalculator.js
@@ -2,8 +2,6 @@ export function add(numbers) {
   if (!numbers) return 0;
 
   const parts = numbers.split(",");
-  const num1 = parseInt(parts[0], 10);
-  const num2 = parseInt(parts[1], 10);
-
-  return num1 + num2;
+  const parsedNumbers = parts.map((num) => parseInt(num, 10));
+  return parsedNumbers.reduce((sum, num) => sum + num, 0);
 }
--- src/utils/stringCalculator.test.js
+++ src/utils/stringCalculator.test.js
@@ -8,4 +8,9 @@ describe("String Calculator", () => {
   test("adds two comma-separated numbers", () => {
     expect(add("1,4")).toBe(5);
   });
+
+  test("adds multiple numbers", () => {
+    expect(add("1,2,3,4,5")).toBe(15);
+    expect(add("10,20,30")).toBe(60);
+  });
 });
